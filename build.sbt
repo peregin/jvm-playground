2214af657a8e8a9503daa4db6b621189b0a3effa
Levente Fall <levente.fall@efgfp.com>
2013-05-30 11:09:00 +0200

    added swing labs dependencies and some components on the test gui

--- build.sbt
+++ build.sbt
@@ -10,3 +10,9 @@ net.virtualvoid.sbt.graph.Plugin.graphSettings
 
 libraryDependencies += "org.scala-lang" % "scala-swing" % "2.10.1"
 
+libraryDependencies += "org.swinglabs" % "swingx-core" % "1.6.2-2"
+
+libraryDependencies += "jgoodies" % "looks" % "2.4.0"
+
+libraryDependencies += "jgoodies" % "common" % "1.1.1"
+
--- src/main/scala/study/gui/Gui.scala
+++ src/main/scala/study/gui/Gui.scala
@@ -1,10 +1,65 @@
 package study.gui
 
-import scala.swing.{MainFrame, Frame, SimpleSwingApplication}
+import scala.swing._
+import scala.collection.mutable.ListBuffer
+import java.awt.Toolkit
+import scala.swing.event.ButtonClicked
 
+case class DataItem(id: Long, name: String)
 
 object Gui extends SimpleSwingApplication {
-  def top: Frame = new MainFrame {
+
+  val items = ListBuffer[DataItem]()
+  (1 to 10).foreach(i => items += DataItem(i, "item %s" format i))
+
+  val testModel = new LabsTableModel(Array("Id", "Name")) {
+    def getRowCount: Int = items.length
+
+    def getValueAt(rowIndex: Int, columnIndex: Int): AnyRef = {
+      val data = items(rowIndex)
+      columnIndex match {
+        case 0 => data.id.asInstanceOf[AnyRef]
+        case 1 => data.name
+        case _ => ""
+      }
+    }
+  }
+
+  val testTable = new LabsTable {
+    model = testModel
+    peer.setVisibleRowCount(10)
+  }
+  val tableScroll = new ScrollPane(testTable)
+  val testButton = new Button("Test")
+
+  val frame = new MainFrame {
     title = "Study"
+    contents = new BorderPanel {
+
+      border = Swing.EmptyBorder(10)
+      import BorderPanel.Position._
+      add(tableScroll, Center)
+      add(testButton, South)
+    }
+  }
+
+  centerWindowToScreen(frame)
+
+  def centerWindowToScreen(window: RichWindow) = {
+    val x = getX(window.size.width)
+    val y = getY(window.size.height)
+    window.location = point(x, y)
+  }
+
+  private def screenSize = Toolkit.getDefaultToolkit().getScreenSize()
+  private def getX(componentWidth: Int) = screenSize.width / 2 - componentWidth / 2
+  private def getY(componentHeight: Int) = screenSize.height / 2 - componentHeight / 2
+  private def point(x: Int, y: Int) = new java.awt.Point(x, y)
+
+  def top = frame
+
+  listenTo(testButton)
+  reactions += {
+    case ButtonClicked(`testButton`) => println("aloha")
   }
 }
