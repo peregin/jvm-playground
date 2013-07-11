ad21db19cbc8539ace989805703227a95f999287
levi <info@peregin.com>
2013-07-11 22:33:23 +0200

    test regex

--- src/main/scala/Hi.scala
+++ src/main/scala/Hi.scala
@@ -1,5 +1,19 @@
 
 
 object Hi extends App {
-  println("hi")
+  for (i <- 0 to (10, 2)) println(i)
+  println("hi----------")
+
+  val pattern = """hello_(t_)(\d+)*(BON)*""".r
+
+  //test("hello")
+  test("hello_t_1234_bing_BONgola")
+
+  def test(in: String) {
+    pattern.findAllIn(in).matchData.foreach{m =>
+      val gc = m.groupCount
+      println(gc)
+      for (i <- 1 to gc) println(m.group(i))
+    }
+  }
 }
