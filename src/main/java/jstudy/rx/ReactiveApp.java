fde0a8d43be46ef2bf9818f225f995ee30f0c8b5
levi <velocorner.com@gmail.com>
2017-03-05 22:04:34 +0100

    java rx with lambda

--- src/main/java/jstudy/rx/ReactiveApp.java
+++ src/main/java/jstudy/rx/ReactiveApp.java
@@ -2,20 +2,15 @@ package jstudy.rx;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 
 public class ReactiveApp {
 
     public static void main(String... args) {
         System.out.println("rx...");
 
-        Observable<String> myObservable = Observable.create(
-                new ObservableOnSubscribe<String>() {
-                    @Override
-                    public void subscribe(ObservableEmitter<String> sub) {
-                        sub.onNext("Hello next");
-                        sub.onComplete();
-                    }
+        Observable<String> myObservable = Observable.create((ObservableEmitter<String> sub) -> {
+                    sub.onNext("Hello next");
+                    sub.onComplete();
                 }
         );
     }
