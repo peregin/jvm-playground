342910a0db19954d01a594df0b9cb471c9c41dc1
Levente Fall <levente.fall@profidatagroup.com>
2017-10-23 14:03:26 +0200

    update kafka host

--- src/main/scala/study/kafka/ConnectivityDetails.scala
+++ src/main/scala/study/kafka/ConnectivityDetails.scala
@@ -3,7 +3,7 @@ package study.kafka
 trait ConnectivityDetails {
 
   //val bootstrapServers = "192.168.0.31:9092,192.168.0.32:9092,192.168.0.33:9092"
-  val bootstrapServers = "192.168.0.40:9092"
-  val zookeeperServers = "192.168.0.40:2181"
+  val bootstrapServers = "192.168.0.20:9092"
+  val zookeeperServers = "192.168.0.20:2181"
   val testTopicName = "x8-topic"
 }
