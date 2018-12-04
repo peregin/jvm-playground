3840300a58a0f6315967d89d413525367663b7f5
levi <velocorner.com@gmail.com>
2018-12-04 08:12:23 +0100

    test

--- src/main/java/study/alg/pointers/Duplicates.java
+++ src/main/java/study/alg/pointers/Duplicates.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class Duplicates {
 
-    public static int removeDuplicates(ArrayList<Integer> a) {
+    public static int removeDuplicates1(ArrayList<Integer> a) {
         int i = 0, k = 0;
         int n = a.size();
         if (n < 2) {
@@ -18,11 +18,33 @@ public class Duplicates {
             }
             i++;
         }
-        return k + 1;
+        return k+1;
+    }
+
+    // can appear max twice
+    public static int removeDuplicates(ArrayList<Integer> a) {
+        if (a.size() <= 2)
+            return a.size();
+
+        int prev = 1; // point to previous
+        int curr = 2; // point to current
+
+        while (curr < a.size()) {
+            if (a.get(curr) == a.get(prev) && a.get(curr) == a.get(prev - 1)) {
+                curr++;
+            } else {
+                prev++;
+                a.set(prev, a.get(curr));
+                curr++;
+            }
+        }
+
+        System.out.println(a);
+        return prev + 1;
     }
 
     public static void main(String[] args) {
-        int res = removeDuplicates(new ArrayList<>(Arrays.asList(3, 3, 5)));
+        int res = removeDuplicates(new ArrayList<>(Arrays.asList(1000, 1000, 1000, 1000, 1001, 1002, 1003, 1003, 1004, 1010)));
         System.out.println("res is "+res);
     }
 }
