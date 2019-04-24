89a9911d6e39b942e762fe7ec6c7e8ca9a02030f
levi <velocorner.com@gmail.com>
2019-04-24 22:37:41 +0200

    test

---
+++ src/main/scala/study/alg/Valleys.scala
@@ -0,0 +1,30 @@
+package study.alg
+
+object Valleys {
+
+  // Complete the countingValleys function below.
+  def countingValleys(n: Int, s: String): Int = {
+    var level = 0
+    var valleys = 0
+    for (c <- s) {
+      c match {
+        case 'U' if level == -1 =>
+          valleys = valleys + 1
+          level = level + 1
+        case 'U' =>
+          level = level + 1
+        case 'D' =>
+          level = level - 1
+        case he =>
+          throw new IllegalArgumentException(s"unexpected character $he")
+      }
+    }
+    valleys
+  }
+
+  def main(args: Array[String]) {
+    val s = "UDDDUDUU"
+    val result = countingValleys(s.length, s)
+    println(s"valleys = $result")
+  }
+}
---
+++ src/main/scala/study/alg/arrays/Socks.scala
@@ -0,0 +1,28 @@
+package study.alg.arrays
+
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Socks {
+
+  // Complete the sockMerchant function below.
+  def sockMerchant(n: Int, ar: Array[Int]): Int = {
+    ar.groupBy(identity).values.map(_.length / 2).sum
+  }
+
+  def main(args: Array[String]) {
+
+    val ar = "10 20 20 10 10 30 50 10 20".split(" ").map(_.trim.toInt)
+    val n = ar.length
+    val result = sockMerchant(n, ar)
+
+    println(result)
+  }
+}
