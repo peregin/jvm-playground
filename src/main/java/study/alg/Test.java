487edc0d1e0719c0fcbaef59cac742ed61d272d9
Levente Fall <levente.fall@tundra.com>
2020-10-04 12:45:33 +0200

    test

--- src/main/java/study/alg/Test.java
+++ src/main/java/study/alg/Test.java
@@ -3,6 +3,10 @@ package study.alg;
 
 public class Test {
 
+    public String calculate(java.io.InputStream in) {
+        return "test";
+    }
+
     public static void main(String[] args) {
         System.out.printf("test");
     }
