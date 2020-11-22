354531511690af2ba0dcc239ff6d299996f06da6
levi <velocorner.com@gmail.com>
2020-11-22 11:09:38 +0100

    prepare tech sessions

--- src/main/scala/teach/TeachFpTopics.scala
+++ src/main/scala/teach/TeachFpTopics.scala
@@ -1,9 +1,10 @@
-package study.teachfp
+package teach
 
 import scalaz.{Cord, Kleisli, Show}
+import study.teachfp.FutureInstances
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * FP sessions + hands on
@@ -12,6 +13,7 @@ import scala.concurrent.Future
  * various manifestations of polymorphism
  * type classes
  * for comprehensions
+ * type variance
  * monoids
  * functors, applicatives
  * validations
@@ -21,24 +23,28 @@ import scala.concurrent.Future
  * writer, state
  * io effects / zio
  * data structures (Nel, Nec, etc)
+ * strictness and laziness
  */
 object TeachFpTopics extends App {
 
   case class Pair[A](p1: A, p2: A) {
     @inline final def map[B](f: A => B) = Pair(f(p1), f(p2))
+
     @inline final def flatMap[B](f: A => Pair[B]) = f
   }
+
   val pair1 = Pair(1, 10)
   val pair2 = Pair("a", "b")
 
   val res = for {
     a1 <- pair1
     a2 <- pair2
-  } yield
+  } yield (a1, a2)
   println(s"pair res is = $res")
 
   // define some models used during the examples
   case class User(id: Long, name: String, age: Int)
+
   case class Car(id: Long, name: String)
 
   val user1 = User(1, "Gandalf", 100)
@@ -46,22 +52,29 @@ object TeachFpTopics extends App {
   val car1 = Car(1, "Honda")
 
   def listCarsFromDb(): Future[Seq[Car]] = Future(Seq(car1))
+
   def getCarFromDb(id: Long): Future[Option[Car]] = listCarsFromDb().map(_.find(_.id == id))
+
   def listUsersFromDb(): Future[Seq[User]] = Future(Seq(user1, user2))
+
   def getUserFromDb(id: Long): Future[Option[User]] = listUsersFromDb().map(_.find(_.id == id))
 
   // example with show
   def show[A: Show](a: A) = implicitly[Show[A]].show(a)
+
   implicit val carShow = new Show[Car] {
     override def show(f: Car): Cord = Cord.stringToCord(f.name)
   }
+
   implicit class ShowOps[A: Show](a: A) {
     def show: Cord = Show[A].show(a)
   }
+
   println(show(car1))
   println(car1.show)
 
   // make a serializer // json/google proto/thrift typeclasses 101
+
   import FutureInstances._
 
   val listUsers: Kleisli[Future, Unit, Seq[User]] = Kleisli(_ => listUsersFromDb())
