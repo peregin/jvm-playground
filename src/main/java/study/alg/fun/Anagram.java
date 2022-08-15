8aeb1d10c79cc11fe6d047fe38f3f2800f8eb918
Levente Fall <levente.fall@tundra.com>
2022-08-15 17:07:26 +0200

    test

---
+++ src/main/java/study/alg/fun/Anagram.java
@@ -0,0 +1,13 @@
+package study.alg.fun;
+
+public class Anagram {
+
+    public static boolean isAnagram(String s) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean result = isAnagram("Taste");
+        System.out.println("Is anagram: " + result);
+    }
+}
